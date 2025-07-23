7a6ebedcbf13bf32d0e1309e4587aeeacd87c1a7
JinKeyp <wlsrkd7312@gmail.com>
2025-07-23 15:10:41 +0900

    푸터 완료

--- ottogether/src/components/footer/Footer.tsx
+++ ottogether/src/components/footer/Footer.tsx
@@ -1,21 +1,21 @@
-
-
-import './Footer.css';
+import S from './Footer.module.css';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-inner">    
-          <button className="footer-logo"><img src="/logo.svg" alt="OTTogether Logo" /></button>          
-        <div className='footer-content'>
-         <p className="footer-team">FES-5-Project2-TEAM-3</p>
-         <ul className="footer-links">
-           <li><a href="#">Privacy Policy</a></li>
-           <li><a href="#">Terms & Conditions</a></li>
-           <li><a href="#">Cookie Policy</a></li>
-           <li><a href="#">Contact</a></li>
-         </ul>
-       </div> 
+    <footer className={S['footer']}>
+      <div className={S['footer-inner']}>
+        <button className={S['footer-logo']}>
+          <img src="/logo.svg" alt="OTTogether Logo" />
+        </button>
+        <div className={S['footer-content']}>
+          <p className={S['footer-team']}>FES-5-Project2-TEAM-3</p>
+          <ul className={S['footer-links']}>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms & Conditions</a></li>
+            <li><a href="#">Cookie Policy</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
