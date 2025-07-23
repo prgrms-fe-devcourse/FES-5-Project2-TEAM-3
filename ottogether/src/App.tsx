5338fdf5fd1f4a9d268c28808bb6b2c34d3e75ab
cheezstick <yooha922@gmail.com>
2025-07-23 12:04:59 +0900

    feat : router 기능 및 NotFound 페이지 (#12)

--- ottogether/src/App.tsx
+++ ottogether/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
-// import {router} from './router'
+import {router} from './router'
 
 
 function App() {
   return (
     <div className='App'>
 			{/* <Header/> */}
-			{/* <RouterProvider router={router}/> */}
+			<RouterProvider router={router}/>
       {/* <Footer/> */}
     </div>
   )
