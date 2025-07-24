50ffc5f5db7917d860f2984e7f60866309cde363
cheezstick <yooha922@gmail.com>
2025-07-24 15:02:39 +0900

    feat : Login - FindId 기능 + UI 구현 1차

--- ottogether/src/router/router.tsx
+++ ottogether/src/router/router.tsx
@@ -3,6 +3,7 @@ import NotFound from '../pages/NotFound/NotFound';
 import Register from '../pages/Register/Register';
 import Home from '../pages/Home/Home';
 import Layout from '../layout/Layout';
+import Login from '../pages/Login/Login';
 
 export const router = createBrowserRouter([
   {
@@ -18,8 +19,8 @@ export const router = createBrowserRouter([
         element: <Register />
       },
       {
-        // path: '/login',
-        // element: <Login />,
+        path: '/login',
+        element: <Login />
       },
     ]
   },
