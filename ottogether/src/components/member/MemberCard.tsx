8240a85eefc150abaf0d14abe6bd4cdda7b5292e
heroson02 <heroson0925@gmail.com>
2025-07-25 12:53:22 +0900

    feat : MemberCard 작성글, 작성 명대사 개수 확인기능 추가

--- ottogether/src/components/member/MemberCard.tsx
+++ ottogether/src/components/member/MemberCard.tsx
@@ -5,23 +5,45 @@ import { useEffect, useState } from "react";
 import { formatDate } from "../../util/formatDate";
 
 type Profile = Tables<'profile'>;
+type ReviewLike = Tables<'review_like'>;
+type QuotesLike = Tables<'quotes_like'>;
 
 function MemberCard() {
 
 	const [profileData, setProfileData] = useState<Profile[] | null>();
+	const [reviewData, setReviewData] = useState<ReviewLike[] | null>();
+	const [quotesData, setQuotesData] = useState<QuotesLike[] | null>();
 
 	useEffect(()=>{
 		async function generateData(){
-			const data = await getData('profile');
-
-			setProfileData(data);
-			console.log('profileData : ', data);
+			setProfileData(await getData('profile'));
+			setReviewData(await getData('review_like'));
+			setQuotesData(await getData('quotes_like'));
 		}
 		generateData()
 	}, [])
 
 	function onClickCell(data : Profile){
 		console.log(data.nickname, ' cell clicked!');
+	}
+
+	function getUserLikeCount(user_id : number, type : "review" | 'quotes') : number{
+		let count = 0;
+
+		if (type === 'review'){
+			reviewData?.forEach(element => {
+				if (element.user_id === user_id)
+					count++;
+		});
+		}
+		else{
+			quotesData?.forEach(element => {
+				if (element.user_id === user_id)
+					count++;
+			});
+		}
+
+		return count;
 	}
 
 	return (
@@ -34,7 +56,7 @@ function MemberCard() {
 					<h2>{data.nickname}</h2>
 					<p><strong>Joined :</strong> {formatDate(data.joined_time)}</p>
 					<p><strong>Favorite Genre :</strong> {data.favorite_genre?.join(', ')}</p>
-					<p><strong>Total Reviews :</strong> 0 | <strong>Total Quotes :</strong> 0 </p>
+					<p><strong>Total Reviews :</strong> {getUserLikeCount(data.user_id, "review")} | <strong>Total Quotes :</strong> {getUserLikeCount(data.user_id, "quotes")} </p>
 				</div>
 					<img className={S.arrow} src='/Chevron_right.svg' alt="rightArrow" />
 			</div>
