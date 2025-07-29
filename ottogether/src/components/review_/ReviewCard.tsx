8c0aa3f60744cea5cf1227ca3cbb0d77635b854a
heroson02 <heroson0925@gmail.com>
2025-07-29 15:45:11 +0900

    feat : ReviewPage 카드 출력

--- ottogether/src/components/review_/ReviewCard.tsx
+++ ottogether/src/components/review_/ReviewCard.tsx
@@ -7,19 +7,27 @@ import { formatDateNoYear } from "../../util/formatDate";
 
 type Review = Tables<'review'>;
 type Comment = Tables<'comment'>;
+type Profile = Tables<'profile'>;
 
 function ReviewCard() {
 	const [reviewData, setReviewData] = useState<Review[] | null>();
 	const [commentData, setCommentData] = useState<Comment[] | null>();
-
+	const [profileData, setProfileData] = useState<Profile[] | null>();
+	
 	useEffect(() => {
 		async function generateData(){
 			const data = await getData('review');
+			const profile = await getData('profile');
 			setReviewData(data);
-			console.log('review : ', data);
+			setProfileData(profile);
+			console.log('profile : ', profile);
 		}
 		generateData();
 	}, []) //TODO : 리뷰가 생성되면 useEffect가 업데이트 되어야 할듯
+
+	function findUserById(inputId : string) : Profile | undefined{
+		return profileData?.find(profile => profile.user_id !== null && profile.user_id === inputId);
+	}
 	
 	return (
 		/* TODO : (element.user_id == 1004)에서 1004 대신 현재 접속중인 유저 ID로 치환하기 */
@@ -28,21 +36,29 @@ function ReviewCard() {
 			reviewData && reviewData.map(element => (
 				<div className={S["card-container"]}>
 				<header>
-					<img src="" alt="profile_image" />
-					<p>{element.user_id} · {formatDateNoYear(element.updated_at!)}</p>
-					{(element.user_id == '1004') && <img src="/YouBadge.svg" alt="isItMeCheck"></img>} 
-					{StarRating(element.rating)}
+					<img className={S['user-avatar']} src={findUserById(element.user_id)?.avatar_url ?? "./beomTeacher.svg"} alt="profile_image" />
+					<p>{findUserById(element.user_id)?.nickname} · {formatDateNoYear(element.updated_at!)}</p>
+					{/* {(element.user_id == '1004') && <img src="/YouBadge.svg" alt="isItMeCheck"></img>}  */}
+					{<img src="/YouBadge.svg" alt="isItMeCheck"></img>} 
+					<div className={S['star-container']}>{StarRating(element.rating)}</div>
 				</header>
 				<main>
 					<p>{element.text_content}</p>
 				</main>
 				<footer>
-					<img src="/thumbsUp.svg" alt="ThumbsUpIcon" />
-					<p>{element.like_count}</p>
-					<img src="/thumbsDown.svg" alt="ThumbsUpIcon" />
-					<p>{element.dislike_count}</p>
-					<img src="/comment.svg" alt="commentIcon" />
-					<p>{element.comment_count}</p>
+					<div className={S['reaction-item']}>
+						<img src="/thumbsUp.svg" alt="ThumbsUpIcon" />
+						<p>{element.like_count}</p>
+					</div>
+					<div className={S['reaction-item']}>
+						<img src="/thumbsDown.svg" alt="ThumbsUpIcon" />
+						<p>{element.dislike_count}</p>
+					</div>
+					<div className={S['reaction-item']}>
+						<img src="/comment.svg" alt="commentIcon" />
+						<p>{element.comment_count}</p>
+					</div>
+					<p className={S['read-more']}>Read More</p>
 				</footer>
 				</div>
 			))
