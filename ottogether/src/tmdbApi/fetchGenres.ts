4aa17c7c299ed71613038adb18b3ed47236b5b72
JinKeyp <wlsrkd7312@gmail.com>
2025-07-30 09:49:05 +0900

    api 에러 수정

--- ottogether/src/tmdbApi/fetchGenres.ts
+++ ottogether/src/tmdbApi/fetchGenres.ts
@@ -5,7 +5,8 @@ import type { Genre } from "./movie.type";
 export async function fetchGenres(): Promise<Genre[]> {
   try {
     const res = await fetch(
-      `https://api.themoviedb.org/3/genre/movie/list?language=ko`,
+      // `https://api.themoviedb.org/3/genre/movie/list?language=ko`,
+      `/api/tmdb/genre/movie/list?language=ko`,
       {
         headers: {
           Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
--- ottogether/src/tmdbApi/getMovieProvider.ts
+++ ottogether/src/tmdbApi/getMovieProvider.ts
@@ -1,6 +1,7 @@
 import type { MovieData } from "./movie.type";
 
-const BASE_URL = "https://api.themoviedb.org/3";
+// const BASE_URL = "https://api.themoviedb.org/3";
+const BASE_URL = "/api/tmdb";
 const TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
 
 export async function getMovieProvider(movies: MovieData[]): Promise<MovieData[]> {
