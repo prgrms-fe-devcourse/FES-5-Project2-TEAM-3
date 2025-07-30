40095346e06d2d5c90d526bcf037c4b3c92f31f5
eafiseemn <eafiseemn@gmail.com>
2025-07-30 17:57:02 +0900

    style : 탭 이동 애니메이션 추가

--- ottogether/src/components/Search/SearchTab.tsx
+++ ottogether/src/components/Search/SearchTab.tsx
@@ -1,3 +1,4 @@
+import gsap from 'gsap';
 import S from './SearchTab.module.css';
 import { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -18,6 +19,9 @@ function SearchTab() {
   const location = useLocation();
   const navigate = useNavigate();
   const [ activeTab, setActiveTab ] = useState<TabType>('total');
+  const tabRefs = useRef<Record<string, HTMLButtonElement | null>>({});
+  const indicatorRef = useRef<HTMLDivElement | null>(null);
+  const panelRef = useRef<HTMLDivElement | null>(null);
 
   /* URL 쿼리에서 초기 상태 설정 */
   useEffect(() => {
@@ -33,8 +37,6 @@ function SearchTab() {
 
   /* 탭 클릭 시 URL 쿼리 변경 */
   const handleTabClick = (tab: TabType) => {
-    const tabRefs = useRef<Record<string, HTMLButtonElement | null>>({});
-
     const params = new URLSearchParams(location.search);
 
     if ( tab === 'total' ) {
@@ -48,9 +50,73 @@ function SearchTab() {
 
   /* 탭 이동 애니메이션 */
   useEffect(() => {
-    // const currentTab = tabRefs.current[activeTab];
-  })
+    // 탭 메뉴 슬라이딩
+    const currentTab = tabRefs.current[activeTab];
+    const indicator = indicatorRef.current;
 
+    if ( currentTab && indicator ) {
+      const { offsetLeft, offsetTop, offsetWidth, offsetHeight } = currentTab;
+      const tabListOffsetTop = (currentTab.offsetParent as HTMLDivElement | null)?.offsetTop ?? 0;
+
+      console.log(offsetTop, offsetHeight, tabListOffsetTop);
+      gsap.to(indicator, {
+        left: offsetLeft,
+        width: offsetWidth,
+        top: offsetTop + offsetHeight - 2 - tabListOffsetTop,
+        duration: 0.1,
+        ease: 'power2.out',
+      })
+    }
+
+    // 패널 슬라이딩
+    const index = TAB_TYPES.indexOf(activeTab);
+    if (panelRef.current) {
+      gsap.to(panelRef.current, {
+        xPercent: -100 * index,
+        duration: 0.1,
+        ease: 'power2.out',
+      })
+    }
+  }, [activeTab]);
+
+  /* 탭별로 표시될 화면 */
+  const resultPanel = (
+      <>
+        <div className={S["panel-item"]}>
+          <div className={S["total-list"]}>
+            <section>
+              <h3>Movies</h3>
+              {/* 영화 리스트 */}
+            </section>
+            <section>
+              <h3>TV Shows</h3>
+              {/* 시리즈 리스트 */}
+            </section>
+            <section>
+              <h3>Quotes</h3>
+              {/* 명대사 리스트 */}
+            </section>
+            <section>
+              <h3>Users</h3>
+              {/* 유저 리스트 */}
+            </section>
+          </div>
+        </div>
+        <div className={S["panel-item"]}>
+          <div className={S["movie-list"]}>Movies</div>
+        </div>
+        <div className={S["panel-item"]}>
+          <div className={S["series-list"]}>Series</div>
+        </div>
+        <div className={S["panel-item"]}>
+          <div className={S["quote-list"]}>Quotes</div>
+        </div>
+        <div className={S["panel-item"]}>
+          <div className={S["user-list"]}>Users</div>
+        </div>
+      </>
+  );
+  
   return (
     <div className={S["search-tab-container"]}>
       <div className={S["tab-list"]} role='tablist' aria-label='검색 카테고리'>
@@ -58,7 +124,7 @@ function SearchTab() {
           <button
             key={tab}
             role='tab'
-            // ref={el => (tabRefs.current[tab] = el)}
+            ref={el => {tabRefs.current[tab] = el}}
             aria-selected={activeTab === tab}
             className={ activeTab === tab ? S.selected : ''}
             onClick={() => handleTabClick(tab)}
@@ -66,36 +132,19 @@ function SearchTab() {
             {TAB_LABELS[tab]}
           </button>
         ))}
+        <div ref={indicatorRef} className={S.indicator}></div>
       </div>
-      <div className={S["search-tab-panel"]} role='tabpanel'>
-        {
-          activeTab === 'total' ? (
-            <>
-              <section>
-                <h3>Movies</h3>
-                {/* 영화 리스트 */}
-              </section>
-              <section>
-                <h3>TV Shows</h3>
-                {/* 시리즈 리스트 */}
-              </section>
-              <section>
-                <h3>Quotes</h3>
-                {/* 명대사 리스트 */}
-              </section>
-              <section>
-                <h3>Users</h3>
-                {/* 유저 리스트 */}
-              </section>
-            </>
-          ) : (
-            <div>
-              {/* 선택한 탭에 따라 필터된 리스트만 렌더링 */}
-            </div>
-          )
-        }
+      <div className={S["search-tab-panel"]} role='tabpanel' ref={panelRef}>
+        { resultPanel }
       </div>
     </div>
   )
 }
 export default SearchTab
+
+
+
+// <div className={S["not-found-list"]}>
+//   <h3>해당하는 검색 결과를 찾을 수 없어요 🥺</h3>
+//   <span>적용된 필터를 제외하거나 검색어를 변경하여 다시 검색해보세요!</span>
+// </div>
