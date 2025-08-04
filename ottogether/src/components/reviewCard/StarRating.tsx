e17f9b31e99580a652fc4f6404c9b1e87758c1d1
heroson02 <heroson0925@gmail.com>
2025-08-04 10:54:40 +0900

    fix : reviewCard 구조 및 css 수정

--- ottogether/src/components/reviewCard/StarRating.tsx
+++ ottogether/src/components/reviewCard/StarRating.tsx
@@ -4,9 +4,9 @@ function StarRating(amount : number){
 		return ;
 	for (let index = 1; index <= 5; index++) {
 		if (index <= amount)
-			ret.push(<img key={index} src="/star/fullStar.svg" alt="starRateImage"></img>)
+			ret.push(<img key={index} style={{width:'24px', height:'24px'}} src="/star/fullStar.svg" alt="starRateImage"></img>)
 		else
-			ret.push(<img key={index} src="/star/emptyStar.svg" alt="starRateImage"></img>)
+			ret.push(<img key={index} style={{width:'24px', height:'24px'}} src="/star/emptyStar.svg" alt="starRateImage"></img>)
 	}
 
 	return (
